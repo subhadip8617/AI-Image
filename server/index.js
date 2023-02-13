3fc12829c5fb3eff13316c22fd31f8c74deb2ca9
Subhadip <subhadip8617@gmail.com>
2023-02-13 13:06:19 +0530

    first commit

---
+++ server/index.js
@@ -0,0 +1,35 @@
+import express from "express";
+import * as dotenv from 'dotenv';
+import cors from 'cors';
+
+import connectDB from './mongodb/connect.js';
+
+import postRoutes from './routes/postRoutes.js';
+import openAiRoutes from './routes/openAiRoutes.js';
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json({limit: '50mb'}));
+
+app.use('/api/v1/post', postRoutes);
+app.use('/api/v1/openAI', openAiRoutes);
+
+app.get('/', async (req, res) => {
+    res.send('Hello From AI');
+})
+
+const startServer = async () => {
+    try {
+        connectDB(process.env.MONGO_URL);
+        app.listen(8080, () => {
+            console.log('Server is running');
+        })
+    } catch (error) {
+        console.log(error);
+    }
+
+}
+
+startServer();
